a64f21c988f60e1cd185a8539c80753c4eaa19ed
n48331 <unfolks@gmail.com>
2022-10-14 23:23:22 +0530

    New version

--- v2/main.py
+++ v2/main.py
@@ -6,6 +6,7 @@ book = openpyxl.load_workbook("./data.xlsx")
 data = book.active
 project_name = data['A2'].value
 source_folder = "./boiler_plate/slide"
+image_folder = "./images"
 shared_src = "./boiler_plate/shared"
 shared_dest = f"./{project_name}/shared"
 config_dest = f"./{project_name}/shared/{project_name}_SharedResource/common"
@@ -19,9 +20,11 @@ for i in range(data.max_row-1):
     shutil.copytree(source_folder, destination_folder)
     createHtml(folder_name, project_name, destination_folder, i+1)
     gCJ(destination_folder)
+# ========= Images ===========
+    shutil.copy(
+        f'{image_folder}/{data[f"C{i+2}"].value}.jpg', f'{destination_folder}/img/main.jpg')
 
 shutil.copytree(shared_src, shared_dest)
 renameSharedFiles(shared_dest, project_name)
 createConfig(project_name, slide_names, config_dest)
-
 print('Folders created')
