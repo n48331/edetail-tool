9e7afb26e55a2a9c597f7da4d83159ff47bb71cb
n48331 <unfolks@gmail.com>
2023-02-12 21:35:16 +0530

    done

---
+++ V2.1 exe/output/11328_Spravato/shared/11328_SharedResource/common/config.js
@@ -0,0 +1,41 @@
+var config = {
+        project: "11328_Spravato",
+        slides: {
+        
+        s1: {
+        name: "s1",
+        zipFile: "11328_Spravato_S1_Slide.zip",
+        },
+        
+        s2: {
+        name: "s2",
+        zipFile: "11328_Spravato_S2_Slide.zip",
+        },
+        
+        s3: {
+        name: "s3",
+        zipFile: "11328_Spravato_S3_Slide.zip",
+        },
+        
+        s4: {
+        name: "s4",
+        zipFile: "11328_Spravato_S4_Slide.zip",
+        },
+        
+        s5: {
+        name: "s5",
+        zipFile: "11328_Spravato_S5_Slide.zip",
+        },
+        
+        },
+        coreflow: {
+            /*First flow should have all the slides*/
+            f0: {
+            content: ['s1', 's2', 's3', 's4', 's5'],
+            name: "Flow 0",
+            },      
+            },
+             "carSlide": ['s2', 's3'],
+            };
+
+	
